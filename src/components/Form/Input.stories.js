838510933b20c52ab8643676be4761fdb7ca831e
Erik Stockmeier <erikds.dsa@gmail.com>
2019-04-07 21:12:08 -0400

    begin inputs

--- src/components/Form/Input.stories.js
+++ src/components/Form/Input.stories.js
@@ -2,12 +2,31 @@ import React from "react"
 
 import { storiesOf, addDecorator } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
-import { Box } from "rebass"
+import { Flex, Box } from "rebass"
 
-import { Input } from "components/Form/Input"
+import { TextInput, BorderedTextInput } from "components/Form/Input"
 
-storiesOf("Form/Input", module).add("Text", () => (
-  <Box width="500px">
-    <Input onClick={action("clicked")} placeholder="Hello" />
-  </Box>
-))
+storiesOf("Form/Input", module)
+  .add("Text", () => (
+    <Flex flexDirection="column" justifyContent="space-between" width="500px">
+      <TextInput onClick={action("clicked")} placeholder="Hello" />
+    </Flex>
+  ))
+  .add("Bordered Text", () => (
+    <Flex flexDirection="column" justifyContent="space-between" width="500px">
+      <Box mb={2}>
+        <BorderedTextInput placeholder="Hello" />
+      </Box>
+      <Box mb={2}>
+        <BorderedTextInput error="Profit is Theft" placeholder="Error" />
+      </Box>
+      <Box>
+        <BorderedTextInput disabled placeholder="Disabled" />
+      </Box>
+    </Flex>
+  ))
+  .add("Bordered Text - Error", () => (
+    <Flex width="500px">
+      <BorderedTextInput error="Profit is Theft" placeholder="Hello" />
+    </Flex>
+  ))
--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,17 +1,40 @@
+import React from "react"
+import { Card, CardProps } from "rebass"
 import styled from "styled-components"
 import { space } from "styled-system"
 
-const Input = styled.input.attrs({ type: "text" })`
+export const TextInput = styled.input.attrs({ type: "text" })`
   border: none;
-  border-bottom: solid 1px ${p => p.theme.colors.inactiveGray};
+  border-bottom: solid 1px ${p => p.theme.colors["gray.3"] || "gray"};
   width: 100%;
   font-size: 0.85rem;
+  font-family: ${p => p.theme.fonts.sans};
   padding-bottom: ${p => p.theme.space[1]};
   ${space}
-
+  &:focus {
+    outline: none;
+  }
   &::placeholder {
-    color: ${p => p.theme.colors.lightGray};
+    color: ${p => p.theme.colors["gray.6"]};
   }
 `
 
-export { Input }
+interface BorderedInputProps extends React.HTMLProps<HTMLButtonElement> {
+  error?: boolean | string
+  borderColor: string
+}
+
+export const BorderedTextInput: React.FunctionComponent<
+  BorderedInputProps & any
+> = ({ error, borderColor = "black", disabled, ...restProps }) => {
+  if (error) {
+    borderColor = "red"
+  } else if (disabled) {
+    borderColor = "gray.5"
+  }
+  return (
+    <Card p={3} borderColor={borderColor} border="2px solid" borderRadius={3}>
+      <TextInput disabled={disabled} {...restProps} />
+    </Card>
+  )
+}
