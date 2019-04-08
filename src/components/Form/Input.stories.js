21b5f25e2a20587b4518b92847ca79de7c043896
Erik Stockmeier <erikds.dsa@gmail.com>
2019-04-08 07:40:55 -0400

    more input styling

--- src/components/Form/Input.stories.js
+++ src/components/Form/Input.stories.js
@@ -4,29 +4,54 @@ import { storiesOf, addDecorator } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import { Flex, Box } from "rebass"
 
-import { TextInput, BorderedTextInput } from "components/Form/Input"
+import { TextInput } from "components/Form/Input"
 
 storiesOf("Form/Input", module)
   .add("Text", () => (
     <Flex flexDirection="column" justifyContent="space-between" width="500px">
-      <TextInput onClick={action("clicked")} placeholder="Hello" />
+      <TextInput
+        onClick={action("clicked")}
+        placeholder="Name"
+        name="input-name"
+        id="input"
+      />
     </Flex>
   ))
   .add("Bordered Text", () => (
     <Flex flexDirection="column" justifyContent="space-between" width="500px">
-      <Box mb={2}>
-        <BorderedTextInput placeholder="Hello" />
+      <Box my={3}>
+        <TextInput
+          label="Name"
+          withBorder
+          placeholder="Hello"
+          id="name"
+          name="name"
+        />
       </Box>
-      <Box mb={2}>
-        <BorderedTextInput error="Profit is Theft" placeholder="Error" />
+      <Box my={3}>
+        <TextInput
+          label="Email"
+          withBorder
+          error="Profit is Theft"
+          placeholder="Error"
+          id="email"
+          name="email"
+        />
       </Box>
-      <Box>
-        <BorderedTextInput disabled placeholder="Disabled" />
+      <Box my={3}>
+        <TextInput
+          id="pwd"
+          name="password"
+          label="Password"
+          withBorder
+          disabled
+          placeholder="Disabled"
+        />
       </Box>
     </Flex>
   ))
-  .add("Bordered Text - Error", () => (
+  .add("Bordered Text - Unlabeled", () => (
     <Flex width="500px">
-      <BorderedTextInput error="Profit is Theft" placeholder="Hello" />
+      <TextInput withBorder type="password" placeholder="Slogan" />
     </Flex>
   ))
