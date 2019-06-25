a1f40cc6b44d15e899888e8f9b464a1ce1fcde2b
Erik Stockmeier <erikds.dsa@gmail.com>
2019-06-25 13:53:18 -0500

    button comments

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -1,10 +1,14 @@
 import React from "react"
-import { Button as ReButton, ButtonProps } from "rebass"
+import { Button as BaseButton, ButtonProps } from "rebass"
 
 // TODO: make disabled button states automatic, rather than variants??
-// Could use rgb/alpha if we wanted...
+// - could use alpha channel on a button variant's background color
+// - could have smart disabled variant`s`, ie
+// <Button variant="primary" disabled ...
+//    ... => <BaseButton variant={disabled ? "disabled" : variant} ...
+//  - This would add requirements to users on their themes though.
 export const Button: React.FunctionComponent<ButtonProps> = props => (
-  <ReButton
+  <BaseButton
     variant="primary"
     borderRadius={3}
     disabled={props.disabled || props.variant === "disabled"}
