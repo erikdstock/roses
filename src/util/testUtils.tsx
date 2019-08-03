a8c070dca0218d6de95a4ed66a72cdfdefcbf93a
Erik Stockmeier <erikds.dsa@gmail.com>
2019-08-03 14:29:50 -0400

    import roses themeprovide directly into testutils for ci

--- src/util/testUtils.tsx
+++ src/util/testUtils.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import { RosesTheme } from "../Theme"
+import { RosesTheme } from "../Theme/ThemeProvider"
 
 export const render = (tree: React.ReactNode) =>
   renderer.create(<RosesTheme>{tree}</RosesTheme>)
